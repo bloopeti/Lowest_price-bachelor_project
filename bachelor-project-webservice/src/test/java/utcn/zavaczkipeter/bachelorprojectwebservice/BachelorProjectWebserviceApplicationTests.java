eb00d030c23cef7494f4b01c4fd503cde6e4731b
Peter Zavaczki <peter.zavaczki@gmail.com>
2019-02-19 11:36:40 +0200

    Clean Spring Initializr webservice project

---
+++ bachelor-project-webservice/src/test/java/utcn/zavaczkipeter/bachelorprojectwebservice/BachelorProjectWebserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package utcn.zavaczkipeter.bachelorprojectwebservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BachelorProjectWebserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
